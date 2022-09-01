5623a5ebedd73e077381f3661ac334477e1cfe60
Chris Culhane <cfculhane@gmail.com>
2022-09-01 14:55:31 +1000

    Disable sentry logging by default, install nvidia drivers in install_os_dependencies.sh

--- install_os_dependencies.sh
+++ install_os_dependencies.sh
@@ -54,6 +54,22 @@ curl -s -L https://nvidia.github.io/nvidia-container-runtime/$distribution/nvidi
 sudo apt update
 sudo apt-get install -y unzip nvidia-container-runtime
 
+if command_exists nvidia-smi; then
+  success_message "NVIDIA drivers already installed"
+else
+  warning_message "NVIDIA drivers could not be found, installing"
+  sudo apt-get install linux-headers-$(uname -r)
+  distribution=$(. /etc/os-release;echo $ID$VERSION_ID | sed -e 's/\.//g')
+  wget https://developer.download.nvidia.com/compute/cuda/repos/$distribution/x86_64/cuda-keyring_1.0-1_all.deb
+  sudo dpkg -i cuda-keyring_1.0-1_all.deb
+  sudo apt-get update
+  sudo apt-get -y install cuda-drivers
+  rm -f cuda-keyring_1.0-1_all.deb
+  success_message "NVIDIA drivers installed!"
+fi
+
+
+
 info_message "Installing / updating docker"
 if command_exists docker; then
   sudo apt-get install -y docker-ce docker-ce-cli containerd.io
@@ -96,5 +112,6 @@ else
   success_message "AWS CLI installed"
 fi
 
-
+info_message "Restarting docker daemon"
+sudo systemctl restart docker
 success_message "Installation of self-hosted os dependencies complete!"
